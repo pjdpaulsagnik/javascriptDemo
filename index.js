3d32349cf9ee2e0756873ffdbc201f45e90aa11b
Jamidarsk <jamidarsk450@gmail.com>
2022-04-06 13:46:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function check()
+{
+    var email = document.getElementById("email").value;
+    console.log("This is the email : ",email);
+}
+
+$(document).ready(function(){
+    $("h2").click(function(){
+      $(this).hide();
+    });
+  });
